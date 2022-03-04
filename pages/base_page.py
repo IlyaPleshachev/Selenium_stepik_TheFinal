82f0bfa84cc587fa0d687a438a82afdbfa10bc1b
IlyaPleshachev <pleshachev@gmail.com>
2022-03-04 16:54:25 +0600

    added test for login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,6 +12,7 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+    
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,8 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_SUBMIT = (By.CSS_SELECTOR, "#login_form > button")
+    REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "#register_form > button")
