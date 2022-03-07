d7a77f09e984876bc7d2912d6d6ef9ef4b3fb32c
IlyaPleshachev <pleshachev@gmail.com>
2022-03-07 15:57:23 +0600

    updated test names in test_product

--- test_product_page.py
+++ test_product_page.py
@@ -25,7 +25,7 @@ class TestUserAddToCartFromProductPage(object):
         cart.should_not_be_success_message()
 
     @pytest.mark.need_review
-    def test_user_can_add_product_to_cart(self, browser):
+    def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         cart = ProductPage(browser, link)
         cart.open()
@@ -54,7 +54,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 
 @pytest.mark.need_review
-def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
